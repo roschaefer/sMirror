ca2be5144d7c8267825a47e0c2ddc968beec054b
Malte Burkhardt <malteburkhardt@gmail.com>
2018-01-03 16:33:49 +0100

    changed path to real feed

--- slave/displays/assets/src/js/tagesschau.js
+++ slave/displays/assets/src/js/tagesschau.js
@@ -1,19 +1,22 @@
 require('../../../node_modules/rss-parser/dist/rss-parser.js');
 
 window.onload = () => {
-    // TODO change url to real feed url - uncomment line below
-    //let url = 'https://www.tagesschau.de/export/video-podcast/tagesschau-in-100-sekunden/';
-    let url = './local-feed.xml';
+    // real feed url - uncomment line below
+    let url = 'https://www.tagesschau.de/export/video-podcast/tagesschau-in-100-sekunden/';
+    // local test feed url
+    //let url = './local-feed.xml';
 
     RSSParser.parseURL(url, (err, parsed) => {
-        if(err){
+        if (err || parsed === undefined) {
             console.error(err);
-        }
-        let template = `
-            <video autoplay="autoplay" controls="controls" width="800" height="480" src="${parsed.feed.entries[0].enclosure.url}"></video>
-        `;
+        } else {
+            let template = `
+                <video autoplay="autoplay" controls="controls" width="800" height="480" src="${parsed.feed.entries[0].enclosure.url}"></video>
+            `;
 
-        document.querySelector('.c-viewport').innerHTML = template;
+            document.querySelector('.c-viewport').innerHTML = template;
+        }
+
     });
 
 };
