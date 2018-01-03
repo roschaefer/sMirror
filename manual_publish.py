7d8a3f568ad3fbf7423859a5e9d7c7715b1031a2
Robert Schaefer <robert.schaefer@student.hpi.de>
2018-01-03 21:51:56 +0100

    Add helper script to publish via mqtt

---
+++ manual_publish.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import paho.mqtt.publish as publish
+import argparse
+parser = argparse.ArgumentParser(description='Publish a url to a display via mqtt')
+parser.add_argument('url', help='the url to publish')
+parser.add_argument('--host', default='192.168.178.118')
+parser.add_argument('--topic', default='slave')
+args = parser.parse_args()
+
+publish.single(args.topic, args.url, hostname=args.host, port=9001, transport="websockets")
+
+
+
