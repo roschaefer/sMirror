c357596b76699f749265719d0f31f7d4dafefa84
Till Prochaska <mail@tillprochaska.de>
2018-01-04 16:10:09 +0100

    Add progress bar animation

--- slave/displays/assets/src/js/countdown.js
+++ slave/displays/assets/src/js/countdown.js
@@ -1,69 +1,17 @@
 import moment from 'moment';
+import Countdown from './lib/countdown.js';
 
 window.onload = () => {
+
     fetch('api.php').then((response) => {
         return response.json();
-    }).then((data) => {
-        //console.log(Date.parse(data['start']));
+    }).then((nextEvent) => {
 
-    let i = 0;
-    if (data) {
-        var countDownDate = Date.parse(data['start']); //new Date("Jan 4, 2018 13:37:25").getTime();
-    }
+        document.querySelector('.c-countdown-display__title').innerHTML = `Zeit bis »${nextEvent.summary}«`;
 
-    let firstNow = new Date().getTime();
-    let firstDistance = countDownDate - firstNow;
+        let nextEventStart = Date.parse(nextEvent.start);
+        new Countdown(document.querySelector('.c-countdown'), nextEventStart);
 
-        window.setInterval(function render() {
-            //let created_at = moment(new Date(tweet.created_at) * 1000).format('DD.MM.YYYY, hh:mm:ss');
-            // Get todays date and time
-            let now = new Date().getTime();
-
-            // Find the distance between now an the count down date
-            let distance = countDownDate - now;
-            var progressBar = document.getElementById("c-countdown-display__bar");
-
-            // Time calculations for days, hours, minutes and seconds
-            let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-            if (!data) {
-                days = 0;
-                hours = 0;
-                minutes = 0;
-                seconds = 0;
-            }
-
-            if (progressBar !== null) {
-                //console.log('test');
-                //console.log(progressBar.style);
-                //progressBar.style.cssText = 'width: 200px;';
-                progressBar.setAttribute('style', 'width: 30%;');
-                // (100 - (distance / firstDistance * 100))
-            }
-            //console.log(progressBar.style.width); //= width + '%'
-
-
-            let template = `
-                        <div class="c-countdown-display">
-                            <div class="c-countdown-display__logo"></div>
-                            <p class="c-countdown-display__count">Countdown</p>
-                            <div class="c-countdown-display__content">
-                                <h1 class="c-countdown-display__subtitle">Zeit bis zum Verlassen des Hauses</h1>
-                                <p class="c-countdown-display__countdown">
-                                    ${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}
-                                </p>
-                                <div id="c-countdown-display__progress">
-                                  <div id="c-countdown-display__bar"></div>
-                                </div>
-                            </div>
-                        </div>`;
-
-            document.querySelector('.c-viewport').innerHTML = template;
-
-        return render;
-    }(), 1000);
     });
+    
 };
