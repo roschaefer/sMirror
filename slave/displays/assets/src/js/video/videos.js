5105596597844e2800875b2a89cb085d5bdd8f5d
Till Prochaska <mail@tillprochaska.de>
2018-01-05 14:06:01 +0100

    Add real video files

---
+++ slave/displays/assets/src/js/video/videos.js
@@ -0,0 +1,44 @@
+module.exports = {
+    begruessungErwachsene: [
+        'Begruessung_Erwachsene_1_Music_GoodMorning_KanyeWest.mp4',
+        'Begruessung_Erwachsene_2_GoodMorning_Sunshine.mp4',
+        'Begruessung_Erwachsene_3_GoodMorning_StevenColbert.mp4',
+        'Begruessung_Erwachsene_4_YouTalkingToMe.mp4',
+    ],
+    begruessungKinder: [
+        'Begruessung_Kinder_1_MausMachtTag.mp4',
+        'Begruessung_Kinder_2_StopmotionLove.mp4',
+    ],
+    tutorialBeautyMen: [
+        'Tutorial_Beauty_Men_1_How to Tie a Tie - Full Windsor Knot.mp4',
+        'Tutorial_Beauty_Men_2_Augenbrauen zupfen.mp4',
+        'Tutorial_Beauty_Men_3_How to Tie a Tie - Full Windsor Knot.mp4',
+        'Tutorial_Beauty_Men_4_Natural MakeUp.mp4',
+        'Tutorial_Beauty_Men_5_How to Tie a Tie - Full Windsor Knot.mp4',
+    ],
+    tutorialBeautyWomen: [
+        'Tutorial_Beauty_Women_1_Bad Hair Day.mp4',
+        'Tutorial_Beauty_Women_2_Augenbrauen routine.mp4',
+        'Tutorial_Beauty_Women_3_Bad Hair Day.mp4',
+        'Tutorial_Beauty_Women_4_Gesichtspflege routine.mp4',
+        'Tutorial_Beauty_Women_5_Bad Hair Day.mp4',
+    ],
+    tutorialGesundheit: [
+        'Tutorial_Gesundheit_1_Gesunde Haltung.mp4',
+        'Tutorial_Gesundheit_2_Tigeratem.mp4',
+        'Tutorial_Gesundheit_3_Lachyoga.mp4',
+        'Tutorial_Gesundheit_4_Gesichtsmassage.mp4',
+        'Tutorial_Gesundheit_5_Schultern Nacken entspannen.mp4',
+        'Tutorial_Gesundheit_1_GOVINDAS LAECHELN.mp4',
+    ],
+    tutorialInspiration: [
+        'Tutorial_Inspiration_1_How to start a movement_800x480.mp4',
+        'Tutorial_Inspiration_2_Got a meeting Take a walk_800x480.mp4',
+        'Tutorial_Inspiration_3_Try something new for 30 days_800x480.mp4',
+        'Tutorial_Inspiration_4_Privilege_800x480.mp4',
+        'Tutorial_Inspiration_5_This is it_800x480.mp4',
+        'Tutorial_Inspiration_6_WhenDidYouChoose_800x480.mp4a',
+    ],
+};
+
+
