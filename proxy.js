4975942cd1d2c465f9c9c015152b21495dd26182
Robert Schaefer <robert.schaefer@student.hpi.de>
2018-01-05 23:51:39 +0100

    Add node shebang

--- proxy.js
+++ proxy.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 // Listen on a specific host via the HOST environment variable
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
