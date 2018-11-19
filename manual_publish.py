69a1ac29cd68d71055ef3e61112edf381d26581b
Robert Schäfer <git@roschaefer.de>
2018-11-19 14:42:09 +0100

    Point to remote mqtt server

--- manual_publish.py
+++ manual_publish.py
@@ -3,11 +3,12 @@ import paho.mqtt.publish as publish
 import argparse
 parser = argparse.ArgumentParser(description='Publish a url to a display via mqtt')
 parser.add_argument('url', help='the url to publish')
-parser.add_argument('--host', default='smirrormaster.local')
+parser.add_argument('--host', default='smirror.canopus.uberspace.de')
+parser.add_argument('--port', default=63126)
 parser.add_argument('--topic', default='slave')
 args = parser.parse_args()
 
-publish.single(args.topic, args.url, hostname=args.host, port=9001, transport="websockets")
+publish.single(args.topic, args.url, hostname=args.host, port=args.port, transport="websockets")
 
 
 
